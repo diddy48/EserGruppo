10752cc1bdc479e894a6108bff7f2c4d5b2cfaad
roman <roman@Gabri>
2017-02-12 11:13:02 +0100

    Second commmit

---
+++ src/romano/Voce.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package romano;
+
+/**
+ *
+ * @author roman
+ */
+class Voce {
+    private String nome;
+    private int numero;
+    public Voce(String nome,int numero){
+        this.nome=nome;
+        this.numero=numero;
+    }
+}
